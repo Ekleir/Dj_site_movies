f026a52c59de84349b8638bd242041d1b9252a4f
Ekleir <vivante1410@gmail.com>
2023-08-06 23:36:32 +0300

    add templates for movies, movie_detail, url + get_absolute_url

--- site_movie/application_movie/views.py
+++ site_movie/application_movie/views.py
@@ -10,3 +10,11 @@ class MovieView(View):
     def get(self, request):
         movies = Movie.objects.all()
         return render(request, 'site_movie/movies.html', {'movie_list': movies})
+
+
+class MovieDetailView(View):
+    """Описание фильма"""
+
+    def get(self, request, slug):
+        movie = Movie.objects.get(url=slug)
+        return render(request, 'site_movie/movie_detail.html', {'movie': movie})
