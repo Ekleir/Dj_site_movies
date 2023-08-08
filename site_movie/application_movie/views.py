7f64491b387c76e1c187782e244585d730c96e5d
Ekleir <vivante1410@gmail.com>
2023-08-09 00:42:19 +0300

    add form for Review & functional on site. add AddReview(View)

--- site_movie/application_movie/views.py
+++ site_movie/application_movie/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import redirect
+from django.views import View
 from django.views.generic import ListView, DetailView
 
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MovieView(ListView):
@@ -17,3 +20,16 @@ class MovieDetailView(DetailView):
     model = Movie
     slug_field = 'url'
     template_name = 'site_movie/movie_detail.html'
+
+
+class AddReview(View):
+    """Отзывы"""
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
+
